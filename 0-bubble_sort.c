f7bfc7cea4193b2def24d590399e6e686bc46c88
Alemi Herbert <alemiherbert@gmail.com>
2023-09-24 19:52:14 +0300

    Track swapped status

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,24 +8,32 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+    size_t i, j;
+    int temp;
+    int swapped; 
 
-	if (size <= 1)
-		return;
+    if (size <= 1)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-		}
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0; 
 
-		print_array(array, size);
-	}
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        print_array(array, size);
+
+        if (swapped == 0)
+            break;
+    }
 }
+
