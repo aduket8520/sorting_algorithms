f2d29eb8b1b9cc1759dc21571b3af68ac104ad8a
titus <titusaduket@gmail.com>
2023-09-24 19:00:08 +0300

    Betty compliance

--- sort.h
+++ sort.h
@@ -15,15 +15,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
-/**
- * this are the helper functions for the code structure
-*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
