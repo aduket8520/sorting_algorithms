bfd6deafe83ee4e982a1615f1e988cc9242ceacc
titus <titusaduket@gmail.com>
2023-09-25 15:58:12 +0300

    correctec the insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,58 +1,48 @@
 #include "sort.h"
 
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *iter, *insert, *tmp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
-
-	while (current != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		listint_t *insertion_point = current->prev;
-		listint_t *next_node = current->next;
-
-		while (insertion_point != NULL && insertion_point->n > current->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			insertion_point = insertion_point->prev;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-
-		if (insertion_point == NULL)
-		{
-			// Move the current node to the front of the list
-			current->prev->next = next_node;
-			if (next_node != NULL)
-				next_node->prev = current->prev;
-			current->next = *list;
-			current->prev = NULL;
-			(*list)->prev = current;
-			*list = current;
-		}
-		else
-		{
-			// Insert the current node after the insertion point
-			current->prev->next = next_node;
-			if (next_node != NULL)
-				next_node->prev = current->prev;
-			current->prev = insertion_point;
-			current->next = insertion_point->next;
-			insertion_point->next = current;
-			if (current->next != NULL)
-				current->next->prev = current;
-		}
-
-		// Print the list after each swap
-		listint_t *temp = *list;
-		while (temp != NULL)
-		{
-			printf("%d", temp->n);
-			if (temp->next != NULL)
-				printf(", ");
-			temp = temp->next;
-		}
-		printf("\n");
-
-		current = next_node;
 	}
 }
